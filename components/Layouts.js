313529f484d4c72e019775dbff516ce5cb6b3a9a
saefulbarkah <saefulbarkah29062003@gmail.com>
2022-12-11 17:19:33 +0700

    update props no margin

--- components/Layouts.js
+++ components/Layouts.js
@@ -10,7 +10,7 @@ const Layouts = ({ children, noMargin }) => {
       <div
         className={`${
           noMargin
-            ? noMargin
+            ? "mt-0 mb-0 my-0 mx-0"
             : "md:ml-[330px] mt-[80px] md:my-[30px] md:mr-[30px] mx-[30px]"
         }`}
       >
--- pages/404.js
+++ pages/404.js
@@ -9,7 +9,7 @@ export default function Custom404() {
   return (
     <>
       <Title name="Page Not Found 404" />
-      <Layouts noMargin="mt-0 mb-0 my-0">
+      <Layouts noMargin="true">
         <div className="h-screen">
           <div className="flex flex-col gap-5 justify-center items-center h-full">
             <Image src="/assets/404.png" width={250} height={250} priority />
