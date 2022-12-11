a9a0f2f19bb40c30572705e9cfdbaaf5759ede1f
saefulbarkah <saefulbarkah29062003@gmail.com>
2022-12-11 17:20:43 +0700

    remove importing useless  module

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import Layouts from "../components/Layouts";
 import Title from "../components/Title";
 
