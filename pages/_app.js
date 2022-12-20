568b2b8503eb8a0914d4338df9f14d882c27a0f0
saefulbarkah <saefulbarkah29062003@gmail.com>
2022-12-20 10:51:36 +0700

    setup ads google

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
+import Script from "next/script";
 import "../styles/globals.css";
 import NextNProgress from "nextjs-progressbar";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "UA-252456907-1");
+  }, []);
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-252456907-1"
+      />
       <NextNProgress color="#FFDB84" options={{ easing: "ease", speed: 500 }} />
       <Component {...pageProps} />
     </>
